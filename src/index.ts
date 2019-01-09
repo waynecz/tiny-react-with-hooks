4a7943f580ac8ebc714b25ed1482d9c7ecc38055
waynecz <451578533@qq.com>
2019-01-09 15:14:09 +0800

    feature: add useEffect

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,88 @@
-import { isClass } from './utils'
-import { patch } from './vnode'
-import { ReactVNODE, FunctionComponent } from './model'
+import { isClass } from "./utils";
+import { patch } from "./vnode";
+import { ReactVNODE, FunctionComponent } from "./model";
 
-// TODO: comment for every variable
-// TODO: make codes human-readable
-let __currentDispatcher = null
+let __ReactCurrentInstance = null;
+let __ReactLatestStateCursor = 0;
+const __ReactMemoizedStates = [];
+const __ReactStateSetters = [];
 
-const memoizedStates = []
-const stateSetters = []
-let cursor = 0
-
-interface memoCursor {
-  value: number
-  __inited: boolean
+interface MemoCursor {
+  value: number;
+  __inited: boolean;
 }
 
 export class Component {
-  private memoStatesCursors: memoCursor[] = []
-  private pointer = -1
-  private renderFunc = null
+  private memoStatesCursors: MemoCursor[] = [];
+  private pointer = -1; // to be added
+  private renderFunc = null;
 
-  public base = null
-  public isReactComponent = {}
+  public base = null;
+  public isReactComponent = {};
 
-  public componentDidMount = null
+  public componentDidMount = null;
+  public componentWillUnmount = null;
 
   constructor(public props = {}) {}
 
   public _render(func) {
-    this.renderFunc = func
-    const vnode = this.renderFunc(this.props)
+    this.renderFunc = func;
+    const vnode = this.renderFunc(this.props);
 
     // every time after finishing render, reset cursor to -1
-    this.pointer = -1
-    return vnode
+    this.pointer = -1;
+    return vnode;
   }
 
   public useState(initialValue) {
-    this.pointer++
+    this.pointer++;
 
     if (!this.memoStatesCursors[this.pointer]) {
-      const memoState = initialValue
+      const memoState = initialValue;
 
-      memoizedStates.push(memoState)
-      stateSetters.push(createStateSetter(cursor, this))
+      __ReactMemoizedStates.push(memoState);
+      __ReactStateSetters.push(
+        createStateSetter(__ReactLatestStateCursor, this)
+      );
 
-      let currentCursor = { value: cursor, __inited: true }
+      let currentCursor: MemoCursor = {
+        value: __ReactLatestStateCursor,
+        __inited: true
+      };
 
-      this.memoStatesCursors.push(currentCursor)
-      cursor++
+      this.memoStatesCursors.push(currentCursor);
+      __ReactLatestStateCursor++;
     }
 
-    const memoCursor = this.memoStatesCursors[this.pointer]
+    const memoCursor = this.memoStatesCursors[this.pointer];
 
-    const getter = memoizedStates[memoCursor.value]
-    const setter = stateSetters[memoCursor.value]
+    const getter = __ReactMemoizedStates[memoCursor.value];
+    const setter = __ReactStateSetters[memoCursor.value];
 
-    return [getter, setter]
+    return [getter, setter];
   }
 
-  public useEffect(callback) {
-    this.componentDidMount = callback
+  public useEffect(
+    componentDidMount: Function,
+    componentWillUnmount?: Function
+  ) {
+    this.componentDidMount = componentDidMount;
+    this.componentWillUnmount = componentWillUnmount;
   }
 }
 
 export function setCurrentDispatcher(instance) {
-  __currentDispatcher = instance
+  __ReactCurrentInstance = instance;
 }
 
-function createStateSetter(cursor, instance) {
+function createStateSetter(__ReactLatestStateCursor, instance) {
   return function(newValue) {
-    memoizedStates[cursor] = newValue
-    const { base, renderFunc } = instance
-    setCurrentDispatcher(instance)
-    const newVnode = instance._render(renderFunc)
-    patch(base, newVnode)
-  }
+    __ReactMemoizedStates[__ReactLatestStateCursor] = newValue;
+    const { base, renderFunc } = instance;
+    setCurrentDispatcher(instance);
+    const newVnode = instance._render(renderFunc);
+    patch(base, newVnode);
+  };
 }
 
 export function createElement(
@@ -84,25 +91,28 @@ export function createElement(
   ...children: []
 ): ReactVNODE {
   if (isClass(type)) {
-    console.warn(`Doesn\'t support class Component: ${type.toString()}`)
-    return
+    console.warn(`Doesn\'t support class Component: ${type.toString()}`);
+    return;
   } else {
     const VDOM = {
       type: type,
       props: props || {},
       children
-    }
+    };
 
-    return VDOM
+    return VDOM;
   }
 }
 
 export function useState(initialValue) {
-  return __currentDispatcher.useState(initialValue)
+  return __ReactCurrentInstance.useState(initialValue);
 }
 
-export function useEffect(callback) {
-  return __currentDispatcher.useEffect(callback)
+export function useEffect(componentDidMount, componentWillUnmount?) {
+  return __ReactCurrentInstance.useEffect(
+    componentDidMount,
+    componentWillUnmount
+  );
 }
 
 export default {
@@ -110,4 +120,4 @@ export default {
   Component,
   useState,
   useEffect
-}
+};
--- src/vnode.ts
+++ src/vnode.ts
@@ -6,9 +6,16 @@ import {
   isEventBinding,
   isObject,
   isEqual
-} from './utils'
-import { ReactVNODE, Primitive, ReactElement } from './model'
-import { Component, setCurrentDispatcher } from '.'
+} from "./utils";
+import { ReactVNODE, Primitive, ReactElement } from "./model";
+import { Component, setCurrentDispatcher } from ".";
+
+function domWillUnmount(dom: ReactElement) {
+  const instance = dom.__react__;
+  if (instance) {
+    instance.componentWillUnmount && instance.componentWillUnmount();
+  }
+}
 
 /**
  * Turn a vnode tree into real DOM and mount it
@@ -17,52 +24,54 @@ export function render(
   vnode: ReactVNODE | Primitive,
   parent: ReactElement | null = null
 ) {
-  const mount = parent ? el => parent.appendChild(el) : el => el
+  const mount = parent ? el => parent.appendChild(el) : el => el;
 
   if (isStrOrNumber(vnode)) {
     // primitive dom render
-    return mount(document.createTextNode(vnode as string))
+    return mount(document.createTextNode(vnode as string));
   } else if (isNegitiveValue(vnode)) {
     // primitive dom render
-    return mount(document.createTextNode(''))
-  } else if (typeof vnode === 'object' && typeof vnode.type === 'string') {
+    return mount(document.createTextNode(""));
+  } else if (typeof vnode === "object" && typeof vnode.type === "string") {
     // vnode tree render
-    const dom = mount(document.createElement(vnode.type as string))
+    const dom = mount(document.createElement(vnode.type as string));
 
     // vnode.children would be a 2 dimension array if children come from a component's children slot,
     flatten(vnode.children).forEach(child => {
-      render(child, dom)
-    })
+      render(child, dom);
+    });
 
     Object.entries(vnode.props).forEach(([attr, value]) => {
-      setAttribute(dom, attr, value)
-    })
+      setAttribute(dom, attr, value);
+    });
 
-    return dom
-  } else if (typeof vnode === 'object' && isFunction(vnode.type)) {
+    return dom;
+  } else if (typeof vnode === "object" && isFunction(vnode.type)) {
     // Render react component
-    const func = vnode.type
-    const props = { ...vnode.props, children: vnode.children }
+    const func = vnode.type;
+    const props = { ...vnode.props, children: vnode.children };
 
-    const instance = new Component(props)
+    const instance = new Component(props);
 
-    setCurrentDispatcher(instance)
+    setCurrentDispatcher(instance);
 
-    const base = render(instance._render(func), parent)
+    const base = render(instance._render(func), parent);
 
-    base.__react__ = instance
-    base.__key__ = vnode.props.key
+    base.__react__ = instance;
+    base.__key__ = vnode.props.key;
 
-    instance.base = base
+    instance.base = base;
 
-    return base
+    instance.componentDidMount && instance.componentDidMount();
+
+    return base;
   } else {
-    console.warn(`ERROR ReactVNODE:`)
-    console.log(vnode)
+    console.warn(`ERROR ReactVNODE:`);
+    console.log(vnode);
   }
 }
 
-const __ReactKeyedElmsPool: Map<string, ReactElement> = new Map()
+const __ReactKeyedElmsPool: Map<string, ReactElement> = new Map();
 
 /**
  * Diff old dom and fresh vnode, then patch
@@ -75,125 +84,127 @@ export function patch(
   vnode: ReactVNODE,
   parent: HTMLElement = null
 ) {
-  const replace = parent ? el => parent.replaceChild(el, dom) && el : el => el
+  const replace = parent ? el => parent.replaceChild(el, dom) && el : el => el;
 
-  if (typeof vnode === 'object' && isFunction(vnode.type)) {
+  if (typeof vnode === "object" && isFunction(vnode.type)) {
     // Function component patch
-    const instance = dom.__react__
+    const instance = dom.__react__;
     if (instance && instance.renderFunc === vnode.type) {
-      const shouldComponentUpdate = !isEqual(instance.props, vnode.children, ['children'])
+      const shouldComponentUpdate = !isEqual(instance.props, vnode.children, [
+        "children"
+      ]);
 
       if (shouldComponentUpdate) {
-        instance.props = Object.assign({}, instance.props, vnode.props)
-        setCurrentDispatcher(instance)
-        const newVnode = instance._render(instance.renderFunc)
+        instance.props = Object.assign({}, instance.props, vnode.props);
+        setCurrentDispatcher(instance);
+        const newVnode = instance._render(instance.renderFunc);
 
-        patch(dom, newVnode)
+        patch(dom, newVnode);
       }
     }
-  } else if (typeof vnode !== 'object' && dom instanceof Text) {
+  } else if (typeof vnode !== "object" && dom instanceof Text) {
     // only text content change
     // 文本节点对比文本节点
-    return dom.textContent === vnode ? dom : replace(render(vnode, parent))
-  } else if (typeof vnode !== 'object' && dom instanceof HTMLElement) {
+    return dom.textContent === vnode ? dom : replace(render(vnode, parent));
+  } else if (typeof vnode !== "object" && dom instanceof HTMLElement) {
     // only text override element
     // 文本节点替代元素
-    const node = document.createTextNode(vnode)
-    return replace(node)
-  } else if (typeof vnode === 'object' && dom instanceof Text) {
+    const node = document.createTextNode(vnode);
+
+    domWillUnmount(dom)
+
+    return replace(node);
+  } else if (typeof vnode === "object" && dom instanceof Text) {
     // element take place of original text
     // 元素替代文本节点
-    return replace(render(vnode, parent))
+    return replace(render(vnode, parent));
   } else if (
-    typeof vnode === 'object' &&
+    typeof vnode === "object" &&
     dom.nodeName !== (vnode.type as string).toUpperCase()
   ) {
     // different type of dom, full rerender and replace
     // 不一样的节点直接视为全替换
-    return replace(render(vnode, parent))
+    return replace(render(vnode, parent));
   } else if (
-    typeof vnode === 'object' &&
+    typeof vnode === "object" &&
     dom.nodeName === (vnode.type as string).toUpperCase()
   ) {
     // the most common scenario, DOM update partially
     // 最常见的情况，节点部分更新，开始做详细的 diff
-    const unkeyedElmsPool: Map<string, ReactElement> = new Map()
+    const unkeyedElmsPool: Map<string, ReactElement> = new Map();
 
-    const oldChildrenNodes = Array.from(dom.childNodes) as ReactElement[]
+    const oldChildrenNodes = Array.from(dom.childNodes) as ReactElement[];
 
     oldChildrenNodes.forEach((child, index) => {
-      const key = child.__key__
+      const key = child.__key__;
 
       if (!key) {
-        const tmpMockKey: string = `INDEX__${index}`
-        unkeyedElmsPool.set(tmpMockKey, child)
+        const tmpMockKey: string = `INDEX__${index}`;
+        unkeyedElmsPool.set(tmpMockKey, child);
       }
-    })
+    });
 
     // 新增和更改 DOM
     flatten(vnode.children).forEach((newChildVnode: ReactVNODE, index) => {
-      const key = newChildVnode.props && newChildVnode.props.key
-      const tmpMockKey: string = `INDEX__${index}`
+      const key = newChildVnode.props && newChildVnode.props.key;
+      const tmpMockKey: string = `INDEX__${index}`;
 
       if (key && __ReactKeyedElmsPool.has(key)) {
-        const newDom = patch(__ReactKeyedElmsPool.get(key), newChildVnode, dom)
-        __ReactKeyedElmsPool.set(key, newDom)
+        const newDom = patch(__ReactKeyedElmsPool.get(key), newChildVnode, dom);
+        __ReactKeyedElmsPool.set(key, newDom);
       } else if (!key && unkeyedElmsPool.has(tmpMockKey)) {
-        patch(unkeyedElmsPool.get(tmpMockKey), newChildVnode, dom)
-        unkeyedElmsPool.delete(tmpMockKey)
+        patch(unkeyedElmsPool.get(tmpMockKey), newChildVnode, dom);
+        unkeyedElmsPool.delete(tmpMockKey);
       } else {
-        render(newChildVnode, dom)
+        render(newChildVnode, dom);
       }
-    })
+    });
 
     // unmount the rest of doms not exist in current vnode.children
     // 删除这次更新不存在于新 children 的
     unkeyedElmsPool.forEach(restElm => {
-      const instance = restElm.__react__
-      if (instance) {
-        instance.componentWillUnmount()
-      }
+      domWillUnmount(restElm)
 
-      restElm.remove()
-    })
+      restElm.remove();
+    });
 
-    patchAttributes(dom, vnode)
+    patchAttributes(dom, vnode);
   }
 }
 
 function patchAttributes(dom: ReactElement, { props: newProps }) {
-  const oldAttrs = {}
+  const oldAttrs = {};
 
   Array.from(dom.attributes).forEach(attr => {
-    oldAttrs[attr.name] = attr.value
-  })
+    oldAttrs[attr.name] = attr.value;
+  });
 
   for (let attrName in oldAttrs) {
-    if (attrName === 'class') {
-      attrName = 'className'
+    if (attrName === "class") {
+      attrName = "className";
     }
     if (!(attrName in newProps)) {
-      setAttribute(dom, attrName, undefined)
+      setAttribute(dom, attrName, undefined);
     }
   }
 
   for (let attrName in newProps) {
-    if (attrName === 'children') continue
+    if (attrName === "children") continue;
 
-    const value = newProps[attrName]
+    const value = newProps[attrName];
 
-    if (attrName === 'className') {
-      attrName = 'class'
+    if (attrName === "className") {
+      attrName = "class";
     }
 
     if (oldAttrs[attrName] !== value) {
-      if (attrName.startsWith('on')) {
+      if (attrName.startsWith("on")) {
         // 如果是事件绑定，先移除旧的
-        const event = attrName.slice(2).toLocaleLowerCase()
-        const oldListener = dom.__listeners__.get(event)
-        dom.removeEventListener(event, oldListener)
+        const event = attrName.slice(2).toLocaleLowerCase();
+        const oldListener = dom.__listeners__.get(event);
+        dom.removeEventListener(event, oldListener);
       }
-      setAttribute(dom, attrName, value)
+      setAttribute(dom, attrName, value);
     }
   }
 }
@@ -207,34 +218,34 @@ function patchAttributes(dom: ReactElement, { props: newProps }) {
 function setAttribute(elm: ReactElement, attr: string, value: any) {
   if (isFunction(value) && isEventBinding(attr)) {
     // event binding
-    const event = attr.slice(2).toLocaleLowerCase()
-    elm.addEventListener(event, value)
+    const event = attr.slice(2).toLocaleLowerCase();
+    elm.addEventListener(event, value);
     if (!elm.__listeners__) {
-      elm.__listeners__ = new Map()
+      elm.__listeners__ = new Map();
     }
 
-    elm.__listeners__.set(event, value)
-  } else if (['checked', 'value', 'className'].includes(attr)) {
+    elm.__listeners__.set(event, value);
+  } else if (["checked", "value", "className"].includes(attr)) {
     // normal attributes
-    elm[attr] = value
-  } else if (attr === 'style' && isObject(value)) {
+    elm[attr] = value;
+  } else if (attr === "style" && isObject(value)) {
     // style assign
-    Object.assign(elm.style, value)
-  } else if (attr === 'ref' && isFunction(value)) {
+    Object.assign(elm.style, value);
+  } else if (attr === "ref" && isFunction(value)) {
     // allow user refer element to their custom variable
     // value: `(elm) => { someVar = elm }` alike
-    value(elm)
-  } else if (attr === 'key') {
+    value(elm);
+  } else if (attr === "key") {
     // self increase key assign
-    ;(elm as any).__key__ = value
-    __ReactKeyedElmsPool.set(value, elm)
+    (elm as any).__key__ = value;
+    __ReactKeyedElmsPool.set(value, elm);
   } else {
     // whatever it be, just set it
-    elm.setAttribute(attr, value)
+    elm.setAttribute(attr, value);
   }
 }
 
 export default {
   render,
   patch
-}
+};
